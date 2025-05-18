ce0d9f90af0d65ba27c049ad3c828ca945de81d1
SabigBenmumin <totpophy14755@gmail.com>
2025-05-18 23:32:37 +0700

    repare

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,19 +5,6 @@ import Header from '../components/Header';
 import OrderItemCard from './components/OrderItemCard';
 import AddOrderButton from './components/AddOrderButton';
 
-async function deleteOrder(id: number) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/order/${id}/`, {
-    method: 'DELETE',
-  });
-
-  if (!res.ok) {
-    throw new Error('Delete order failed!');
-  }
-
-  
-  return true;
-}
-
 // async function getOrder() {
 //   console.log('getorder')
 //   const res = await fetch(`http://127.0.0.1:8000/api/order`)
